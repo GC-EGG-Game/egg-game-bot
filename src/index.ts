849d44d227f02d3dbd389bf325ef70d668bd4ecc
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-12-11 02:41:01 +0900

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Markup, Telegraf } from "telegraf";
+import { config } from "dotenv";
+
+const main = async () => {
+  config();
+
+  const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+  bot.command("start", async (ctx) => {
+    await ctx.reply("GOSTON : Battle in Korea", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "게임 시작!",
+              web_app: { url: process.env.WEB_APP_URL! },
+            },
+          ],
+        ],
+      },
+    });
+  });
+
+  bot.launch();
+
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+};
+
+main();
